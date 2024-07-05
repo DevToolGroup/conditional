bf9ebe66e80296d2a1af1b96c378a9c507e0e3ea
GroupDevTool <haveabigdream@126.com>
2024-07-05 15:26:14 +0800

    增加GPL-3.0-License

--- engine/src/main/java/group/devtool/conditional/engine/RuleClassException.java
+++ engine/src/main/java/group/devtool/conditional/engine/RuleClassException.java
@@ -1,3 +1,13 @@
+/*
+ * The Conditional rule engine, similar to Drools, 
+ * introduces the definition of input and output parameters, 
+ * thereby demarcating the boundaries between programmers and business personnel. 
+ * 
+ * It reduces the complexity of rules, making it easier for business staff to maintain and use them.
+ *
+ * License: GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007
+ * See the license.txt file in the root directory or see <http://www.gnu.org/licenses/>.
+ */
 package group.devtool.conditional.engine;
 
 public class RuleClassException extends Exception {
